ac23c2ad2ed45885200939316508ea31b271ca44
Cole MacLean <maclean.cole@gmail.com>
2016-11-02 20:05:48 +0100

    changed development settings for deploy

--- planning_exercise/app.py
+++ planning_exercise/app.py
@@ -49,11 +49,11 @@ if __name__ == "__main__":
     port = 8000
 
     # Open a web browser pointing at the app.
-    os.system("open http://localhost:{0}/".format(port))
+    #os.system("open http://localhost:{0}/".format(port))
 
     # Set up the development server on port 8000.
-    app.debug = True
-    app.run(port=port)
+    app.debug = False
+    app.run()
 
 
 
